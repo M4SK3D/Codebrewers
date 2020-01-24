80fda768c60c6c64dfdf4b1b8791984935e00a0a
Kailash Ramesh <r.k.kailash@hotmail.com>
2020-01-24 10:16:28 +0530

    Updates

---
+++ precache-manifest.1599c7e83c750f7d2943224a363e5d34.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6ca989b11c515e8801570183391243f",
+    "url": "/Codebrewers/index.html"
+  },
+  {
+    "revision": "5c0d28b7241975a78782",
+    "url": "/Codebrewers/static/css/main.a5968fce.chunk.css"
+  },
+  {
+    "revision": "56cc6dc375c647c3dd79",
+    "url": "/Codebrewers/static/js/2.2310a228.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Codebrewers/static/js/2.2310a228.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5c0d28b7241975a78782",
+    "url": "/Codebrewers/static/js/main.322d0943.chunk.js"
+  },
+  {
+    "revision": "adbc18b8f9a20e2bc9d4",
+    "url": "/Codebrewers/static/js/runtime-main.9f35241f.js"
+  }
+]);
