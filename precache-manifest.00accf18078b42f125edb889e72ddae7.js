966b028a892e5f12907e851f512c9150a4bbec33
Kailash Ramesh <r.k.kailash@hotmail.com>
2020-01-24 10:04:57 +0530

    Updates

---
+++ precache-manifest.00accf18078b42f125edb889e72ddae7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69775ca551a5252c49523251c1c1fa7b",
+    "url": "/website/index.html"
+  },
+  {
+    "revision": "c79a92ff2042f3ec3775",
+    "url": "/website/static/css/main.a5968fce.chunk.css"
+  },
+  {
+    "revision": "56cc6dc375c647c3dd79",
+    "url": "/website/static/js/2.2310a228.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/website/static/js/2.2310a228.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c79a92ff2042f3ec3775",
+    "url": "/website/static/js/main.0ef18d8d.chunk.js"
+  },
+  {
+    "revision": "2e566c3a9d290c8c04a1",
+    "url": "/website/static/js/runtime-main.f854fce0.js"
+  }
+]);
